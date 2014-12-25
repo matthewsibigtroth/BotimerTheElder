98c6702b0f35c8f0d33c10f07a1277eb82512890
Matthew Sibigtroth <matthew.sibigtroth@gmail.com>
2014-12-25 10:36:51 -0800

    create knower fragment

---
+++ app/src/main/java/com/sibigtroth/botimer/KnowerFragment.java
@@ -0,0 +1,22 @@
+package com.sibigtroth.botimer;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class KnowerFragment extends Fragment {
+
+  public KnowerFragment() {
+
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_knower, container, false);
+  }
+
+
+}
